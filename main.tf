a65c288deede46892ee201c49b06a6f87d3fce18
Guy Wald <guy>
2019-01-08 17:40:03 +0200

    changing codecommit resource to data

--- main.tf
+++ main.tf
@@ -21,10 +21,8 @@ module "unique_label" {
 }
 
 # CodeCommit resources
-resource "aws_codecommit_repository" "repo" {
+data "aws_codecommit_repository" "repo" {
   repository_name = "${var.repo_name}"
-  description     = "${var.repo_name} repository."
-  default_branch  = "${var.repo_default_branch}"
 }
 
 # CodePipeline resources
